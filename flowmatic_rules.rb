5b91343a101d666fbaa863aa61e8647f5b03b794
Ahkeem Lang <langahkeem@gmail.com>
2016-11-15 11:49:05 -0600

    Adding more to my conditional statement

--- flowmatic_rules.rb
+++ flowmatic_rules.rb
@@ -1,9 +1,21 @@
 flowmatic_on = true
 water_available = true
 if flowmatic_on && water_available
-    flow_rate = 50
-  elsif !flowmatic_on
-    puts "Flowmatic is off!"
-  else
-    puts "No water!"
+  flow_rate = 50
+elsif !flowmatic_on
+  puts "Flowmatic is off!"
+else
+  puts "No water!"
+end
+
+if flow_rate > 50
+  puts "Warning! flow_rate is above 50! It's #{flow_rate}"
+  flow_rate = 50
+  puts "The flow_rate's been rest to #{flow_rate}."
+elsif flow_rate < 50
+  puts "Warning! flow_rate is below 50! It's #{flow_rate}"
+  flow_rate = 50
+  puts "The flow_rate's been reset to #{flow_rate}."
+else
+  puts "The flow_rate is #{flow_rate} (Thank goodness)."
 end
