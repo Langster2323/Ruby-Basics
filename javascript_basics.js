c1b40a12057e0dcd4802cf4a221654773ff632d6
Ahkeem Lang <langahkeem@gmail.com>
2016-11-28 12:50:22 -0600

    REwriting comments to become more comprehensible.

--- javascript_basics.js
+++ javascript_basics.js
@@ -2,7 +2,10 @@ a = 21;
 b = a * 2;
 console.log(b);
 alert(b);
-// A brief example of a output, arthmetic, assignment, literal and variable expressions.
+/*
+ A brief example of a output, arthmetic, assignment,
+ literal and variable expressions.
+*/
 
 age = prompt("Please enter your age:");
 console.log(age);
